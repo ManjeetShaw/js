25b9db733878be32aec60c2035834f00b6645a1b
Manjeet <manjeetshaw07@gmail.com>
2026-04-15 22:28:28 +0530

    leaned al the functions

---
+++ 01_basics/07_date.js
@@ -0,0 +1,23 @@
+let myDate = new Date()
+
+
+console.log(myDate.toString());
+console.log(myDate.toDateString);
+console.log(myDate.toLocaleString);
+console.log(myDate.toLocaleDateString);
+console.log(myDate.toISOString);
+console.log(myDate.toJSON);
+console.log(myDate.toLocaleTimeString);
+console.log(myDate.toUTCString);
+console.log(myDate.getTimezoneOffset);
+console.log(myDate.getHours);
+console.log(myDate.getUTCHours);
+console.log(myDate.setHours);
+console.log(myDate.setMonth);
+console.log(myDate.setUTCSeconds)
+
+
+
+
+
+
