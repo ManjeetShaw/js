0519a1ad7e9c5211700c761ee213d58936ec690e
Manjeet <manjeetshaw07@gmail.com>
2026-05-08 00:00:12 +0530

    random boolean array

--- 01_basics/revision.js
+++ 01_basics/revision.js
@@ -1,6 +1,9 @@
 // const randomBool = Math.random() < 0.5;
 // console.log(randomBool);
 
+
+
+
 // coin flip
 // function coinFlip() {
 //   return Math.random() < 0.5 ? "Heads" : "Tails";
@@ -8,9 +11,20 @@
 // console.log(coinFlip());
 
 
-// Custom probability (e.g. 70% chance of true)
-function randomChance(percent) {
-  return Math.random() < percent / 100;
-}
-console.log(randomChance(70)); // true ~70% of the time
-console.log(randomChance(10)); // true ~10% of the time
+
+
+
+// // Custom probability (e.g. 70% chance of true)
+// function randomChance(percent) {
+//   return Math.random() < percent / 100;
+// }
+// console.log(randomChance(70)); // true ~70% of the time
+// console.log(randomChance(10)); // true ~10% of the time
+
+
+
+
+// Random boolean array
+const boolArray = Array.from({ length: 5 }, () => Math.random() < 0.5);
+console.log(boolArray); // e.g. [true, false, true, true, false]
+
