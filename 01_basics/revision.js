7bcb7573e12f60ca186ee3dd6fdf44d21844dc06
Manjeet <manjeetshaw07@gmail.com>
2026-05-07 23:52:49 +0530

    changing the file space

--- 01_basics/revision.js
+++ 01_basics/revision.js
@@ -1,2 +1,9 @@
-const randomBool = Math.random() < 0.5;
-console.log(randomBool);
+// const randomBool = Math.random() < 0.5;
+// console.log(randomBool);
+
+// coin flip
+function coinFlip() {
+  return Math.random() < 0.5 ? "Heads" : "Tails";
+}
+console.log(coinFlip());
+
